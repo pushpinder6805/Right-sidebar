a6d564e4490ce033971ab976c4b680f3c1a423ff
pushpender13 <52063297+pushpinder6805@users.noreply.github.com>
2024-02-12 13:20:12 +0530

    Create tc-right-sidebar.js

---
+++ javascripts/connectors/before-list-area/tc-right-sidebar.js
@@ -0,0 +1,38 @@
+import discourseComputed from "discourse-common/utils/decorators";
+import { inject as service } from "@ember/service";
+
+export default {
+  setupComponent() {
+    this.reopen({
+      router: service(),
+
+      @discourseComputed(
+        "router.currentRouteName",
+        "router.currentRoute.attributes.category.slug",
+        "router.currentRoute.attributes.tag.id"
+      )
+      showSidebar(currentRouteName, categorySlug, tagId) {
+        if (this.site.mobileView) {
+          return false;
+        }
+
+        if (settings.show_in_routes !== "") {
+          const selectedRoutes = settings.show_in_routes.split("|");
+
+          if (
+            selectedRoutes.includes(currentRouteName) ||
+            selectedRoutes.includes(`c/${categorySlug}`) ||
+            selectedRoutes.includes(`tag/${tagId}`)
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        // if theme setting is empty, show everywhere except /categories
+        return currentRouteName === "discovery.categories" ? false : true;
+      },
+    });
+  },
+};
